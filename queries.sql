e03ffc19fc59d04dbb3663e269e1e624d41ae223
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-04 23:46:44 +0000

    Insert data into animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,13 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+INSERT INTO ANIMALS (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon', '02-03-2020', 0, true, 10.23);
+
+INSERT INTO ANIMALS (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Gabumon', '11-15-2018', 2, true, 8);
+
+INSERT INTO ANIMALS (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Pikachu', '01-07-2021', 1, false, 15.04);
+
+INSERT INTO ANIMALS (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Devimon', '05-12-2017', 5, true, 11);
