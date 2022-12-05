1128c2ab86a4c65837e86738c0143b6a96e46054
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-05 00:20:10 +0000

    Find all animals that are neutered.

--- queries.sql
+++ queries.sql
@@ -9,3 +9,5 @@ SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered IS TRUE;
