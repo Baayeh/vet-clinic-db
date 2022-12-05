d122a2f6d09385ce5ed593b1e49cc1c456a5840c
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-05 00:18:02 +0000

    List name and escape attempts of animals that weigh more than 10.5kg

--- queries.sql
+++ queries.sql
@@ -7,3 +7,5 @@ SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-3
 SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
