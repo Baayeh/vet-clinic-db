87bdfcda6d960d323098c3b1ad42c21c176406d9
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-05 00:02:19 +0000

    List the name of animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from ANIMALS WHERE name LIKE '%mon'
+SELECT * from ANIMALS WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
