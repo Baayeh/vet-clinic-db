0d6507b2e77a36a21271a6e251f4b71dcc7c201e
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-12-07 17:25:02 +0000

    Insert data into vets table

--- data.sql
+++ data.sql
@@ -108,3 +108,12 @@ WHERE name IN ('Angemon', 'Boarmon');
 
 --- commit transaction
 COMMIT;
+
+
+/* INSERT DATA into vets table */
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
+
+/* INSERT DATA into specializations table */
+/* INSERT DATA into specializations table */
